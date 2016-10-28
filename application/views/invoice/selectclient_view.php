e4251e75c1c2391ba36bf96de40b89ef14eef02c
bamidele <bamidele@Avila_Laptop4>
2016-10-28 09:56:52 +0100

    i have added select client page, edited reel.css

---
+++ application/views/invoice/selectclient_view.php
@@ -0,0 +1,37 @@
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>
+            New Invoice
+        </h1>
+    </section>
+    <section class="content">
+        <!-- Client list drop down box -->
+        <div class="box box-default-client">
+            <div class="box-body with-border">
+                <h3 class="box-body text-center">Please choose the client you are sending an invoice to</h3>
+            </div>           
+            <div class="box-body">
+                <div class="row">
+                    <div class="col-md-6" style="float: none; margin: 0px auto">
+                        <div class="form-group">
+                            <select class="form-control select2" style="width: 100%;">
+                                <option selected="selected">Sample Client</option>
+                                <option>Bluth Company</option>
+                                <option>Virtucon</option>
+                                <option>Widget Corp</option>
+                                <option>Smith & Co.</option>
+                                <option>Texas</option>
+                                <option>Western Gas & Electric</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="select-client-next-button">
+                <button type="button" class="btn btn-primary">Next</button>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
